908d1e0c12c5f6e496bc4cd8b323a7312a01a86a
qflurryy <mcflurry.ff@outlook.com>
2026-02-05 21:19:23 +0700

    fetch.js

---
+++ fetch.js
@@ -0,0 +1,18 @@
+const fetchGitHubJson = async (url) => {
+
+  const rawUrl = url.replace('github.com', 'raw.githubusercontent.com').replace('/blob/', '/');
+
+  try {
+    const response = await fetch(rawUrl);
+    
+    if (!response.ok) throw new Error(`Status: ${response.status}`);
+
+    const data = await response.json();
+  
+    console.log(data); 
+  } catch (error) {
+    console.error("err:", error.message);
+  }
+};
+
+fetchGitHubJson('https://github.com/qflurryy/POST/blob/main/amipretty.json');
